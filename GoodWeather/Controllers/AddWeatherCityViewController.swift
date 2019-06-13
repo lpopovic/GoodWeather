7b3341955e469025cb9f20765110408b2cdb5ac7
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-13 14:58:16 +0200

    init Controllers

---
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by MacBook Air on 6/13/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by MacBook Air on 6/13/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
