b51ef474499987ea02a772f1d38df6a390ac0e9f
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-13 16:01:41 +0200

    Add Weather Cell

--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -15,5 +15,27 @@ class WeatherListTableViewController: UITableViewController {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
+     
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        
+        cell.cityNameLabel.text = "Houston"
+        cell.temperaturaLabel.text = "70°"
+        
+        return cell
     }
 }
---
+++ GoodWeather/CustomCell/WeatherCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by MacBook Air on 6/13/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperaturaLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
