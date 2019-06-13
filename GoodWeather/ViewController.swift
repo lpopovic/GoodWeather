2ce5813db01022175fe48049247ebe6f3792ef52
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-13 11:53:27 +0200

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by MacBook Air on 6/13/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
